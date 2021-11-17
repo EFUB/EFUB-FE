acc60bf405f732e4f5b64f14b742e3c114b081d8
thals1214 <thaxls@ewhain.net>
2021-11-17 17:03:51 +0900

    버튼크기 수정

--- efub-homepage/src/pages/DeveloperRecruitLead.js
+++ efub-homepage/src/pages/DeveloperRecruitLead.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import palette from "../lib/styles/palette";
-import Button from "../components/common/Button";
-import Input_box from "../components/common/Input";
-import { RadioButton } from "../components/common/RadioButton";
-import Checkbox from "../components/common/CheckBox";
-
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import palette from '../lib/styles/palette'
+import Button from '../components/common/Button'
+import Input_box from '../components/common/Input'
+import { RadioButton } from '../components/common/RadioButton'
+import Checkbox from '../components/common/CheckBox'
 
 const Banner = styled.div`
   height: 15rem;
@@ -14,7 +13,7 @@ const Banner = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const Main = styled.div`
   display: flex;
@@ -23,7 +22,7 @@ const Main = styled.div`
   //justify-content: center;
   width: 35rem;
   height: 50rem;
-`;
+`
 const Foot = styled.div`
   width: 35rem;
   display: flex;
@@ -31,21 +30,21 @@ const Foot = styled.div`
   align-items: center;
   margin-top: 2.5rem;
   justify-content: space-between;
-`;
+`
 const FootSub = styled.div`
-  width: 47%;
+  width: 49%;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-`;
+`
 const TextBox = styled.div`
   width: 100%;
   height: 9rem;
   border-radius: 14px;
   background-color: rgba(225, 225, 225, 0.15);
   margin-bottom: 2.5rem;
-`;
+`
 const Text1 = styled.div`
   font-family: Roboto;
   font-style: normal;
@@ -53,14 +52,14 @@ const Text1 = styled.div`
   font-size: 1rem;
   margin-bottom: 1rem;
   line-height: 1.725rem; //28px
-`;
+`
 const Text2 = styled.div`
   font-family: Roboto;
   font-style: normal;
   font-weight: 500;
   font-size: 0.8rem;
   line-height: 1.725rem; //28px
-`;
+`
 const Input = styled.input`
   font-size: 1rem;
   font-weight: 300;
@@ -72,7 +71,7 @@ const Input = styled.input`
   padding-bottom: 0.5rem;
   margin-bottom: 2.5rem;
   width: 100%;
-`;
+`
 const Row = styled.div`
   display: flex;
   flex-direction: row;
@@ -80,70 +79,71 @@ const Row = styled.div`
   justify-content: space-between;
   width: 100%;
   height: 4rem;
-`;
+`
 const Label1 = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    width: 50%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 50%;
 `
 const Labelsub = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: ;
-    justify-content: flex-start;
+  display: flex;
+  flex-direction: column;
+  //align-items: ;
+  justify-content: flex-start;
 `
 const LabelText = styled.div`
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 1rem;
-    margin-left: 1.3rem;
-    line-height: 1.2rem; 
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1rem;
+  margin-left: 1.3rem;
+  line-height: 1.2rem;
 `
 
 const DeveloperRecruitLead = () => {
-  const [checked, setChecked] = useState(false);
-  const handleCheckboxChange = (e) => {
-    setChecked(e.target.checked);
-  };
+  const [checked, setChecked] = useState(false)
+  const handleCheckboxChange = e => {
+    setChecked(e.target.checked)
+  }
   return (
     <div>
       <Banner>
-        <h1 style={{ height: "3.5rem", marginBottom: "0.01rem" }}>
+        <h1 style={{ height: '3.5rem', marginBottom: '0.01rem' }}>
           LEAD DEVELOPER
         </h1>
-        <h6 style={{ marginTop: "0.001rem" }}>지원서 작성</h6>
+        <h6 style={{ marginTop: '0.001rem' }}>지원서 작성</h6>
       </Banner>
       <Main>
         <Text1>1. 지금까지의 프로젝트 경험를 서술해주세요. (500자 내외)</Text1>
         <Input_box
-              placeholder="내용을 입력해주세요."
-              name="project_experience"
-              width="35"
-              inputwidth = "95"
-              height="10"
-              marginTop="13"
-              validinput="true"
-            />
-        <Text1 style = {{marginTop: "3rem"}}
-        >2. 깃허브 또는 기술블로그 링크를 적어주세요.</Text1>
+          placeholder="내용을 입력해주세요."
+          name="project_experience"
+          width="35"
+          inputwidth="95"
+          height="10"
+          marginTop="13"
+          validinput="true"
+        />
+        <Text1 style={{ marginTop: '3rem' }}>
+          2. 깃허브 또는 기술블로그 링크를 적어주세요.
+        </Text1>
         <Input />
         <Text1>
           3. 세미나와 프로젝트에 참가 가능한지, 공지사항에 있는 모든 일정을
           확인하셨습니까?
         </Text1>
-        <label style={{ marginBottom: "2rem", marginTop: "1rem" }}>
+        <label style={{ marginBottom: '2rem', marginTop: '1rem' }}>
           <Checkbox checked={checked} onChange={handleCheckboxChange} />
           <span>&ensp;&ensp;&ensp;&ensp;네 확인했습니다.</span>
         </label>
-        <Text1 style={{ marginBottom: "0rem" }}>
-          4. 다음중 면접이 '불가능한' 시간을 선택해주세요.{" "}
+        <Text1 style={{ marginBottom: '0rem' }}>
+          4. 다음중 면접이 '불가능한' 시간을 선택해주세요.{' '}
         </Text1>
         <Text2>
           불가능한 시간을 모두 선택해주시고, 모두 가능하다면 '모두 가능합니다'를
-          선택해주세요.{" "}
+          선택해주세요.{' '}
         </Text2>
         <Row style={{ marginTop: '1.5rem' }}>
           <Label1>
@@ -210,7 +210,7 @@ const DeveloperRecruitLead = () => {
         </Foot>
       </Main>
     </div>
-  );
-};
+  )
+}
 
-export default DeveloperRecruitLead;
+export default DeveloperRecruitLead
