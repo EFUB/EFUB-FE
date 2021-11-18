72fc4145b32d840a40e07e3956c99412150a15ec
Haerin Lee <dazzlynnnn@gmail.com>
2021-11-18 17:27:27 +0900

    Input 값 받아오기

--- efub-homepage/src/pages/ContactPage.js
+++ efub-homepage/src/pages/ContactPage.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
-import Input from "../components/common/InputBox";
+import InputBox from "../components/common/InputBox";
 import Button from "../components/common/Button";
 import Banner from '../components/contact/Banner';
 
@@ -16,21 +16,50 @@ const Label = styled.div`
 `;
 
 const ContactPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const [inputs, setInputs] = useState({
+    email: '',
+    message: '',
+  });
+
+  const { email, message } = inputs;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
   return (
     <>
       <Banner />
       <FormBox>
         <Label>Email</Label>
-        <Input
+        <InputBox
+          name="email"
+          value={email}
+          onChange={onChange}
           placeholder="답변 받을 이메일 주소 입력"
           height="5.5rem"
         />
         <Label>문의 내용</Label>
-        <Input
+        <InputBox
+          name="message"
+          value={message}
+          onChange={onChange}
           placeholder="EFUB과 협업하고 싶어요! / 지원 기준이 어떻게 되나요?"
         />
       </FormBox>
-      <Button>전송하기</Button>
+      <Button
+        width="10"
+        style={{ marginBottom: "10rem" }}
+        onClick={() => { alert(`email : ${email} / message: ${message}`) }}
+      >전송하기</Button>
     </>
   );
 };
