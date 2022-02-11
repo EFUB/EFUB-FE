495cee583c859cf038e03f0238ad805e44db5602
Haerin Lee <dazzlynnnn@gmail.com>
2022-02-11 15:55:39 +0900

    리크루팅 준비

--- efub-homepage/src/lib/api/client.js
+++ efub-homepage/src/lib/api/client.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 const client = axios.create();
-client.defaults.baseURL = 'http://52.79.53.200:8080/api/';
+client.defaults.baseURL = 'http://13.125.136.136:8080/api/';
 export default client;
